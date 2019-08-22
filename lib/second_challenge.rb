a03cf051601d4c33abd1464e05c6edb2e2818510
Matt Plichta <bigcatplichta@gmail.com>
2019-08-22 01:12:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {
+        name: "Lord Montague",
+        age: "53"
+      },
+      :matriarch => {
+        name: "Lady Montague",
+        age: "54"
+      },
+      :hero => {
+        name: 
+      },
       :hero_friends => []
       }, 
     :capulet => {
