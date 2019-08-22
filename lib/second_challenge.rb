22a1d71a473df9109d66993afcf6de51fe325919
Matt Plichta <bigcatplichta@gmail.com>
2019-08-22 01:24:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -14,8 +14,13 @@ def second_challenge
         age: "15",
         status: "alive"
       },
-      :hero_friends => []
-      }, 
+      :hero_friends => [
+        { name: "Benvolio",
+          age: "17",
+          attitude: "worried"},
+        { name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"}]
     :capulet => {
       :patriarch => {
         name: "Lord Capulet",
@@ -30,8 +35,12 @@ def second_challenge
         age: "15",
         status: "alive"
       },
-      :heroine_friends => []
-      }}
-
-  
+      :heroine_friends => [
+        { name: "Steven",
+          age: "30",
+          attitude: "confused"},
+        { name: "Nurse",
+          age: "44",
+          attitude: "worried"}]
+      
 end
