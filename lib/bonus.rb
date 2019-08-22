344a6d6d079f8de0d9cd4d060b5f9e4858ff3887
Matt Plichta <bigcatplichta@gmail.com>
2019-08-22 01:43:15 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
- epic_tragedy[:Montague][:hero][:status] = "dead as fuck"
- epic_tragedy[:Capulet][:heroine][:status] = "dead as fuck"
+ epic_tragedy[:montague][:hero][:status] = "dead as fuck"
+ epic_tragedy[:capulet][:heroine][:status] = "dead as fuck"
 
   
 
